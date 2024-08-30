638c5eee984d30cefda5fd4256b9f9fc826e311d
offlineprogrammer <mmalaka@gmail.com>
2024-08-30 10:11:44 -0700

    Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 "use client";
-import { Button } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  Button,
+  Icon,
+  View,
+  Heading,
+  Flex,
+  Text,
+} from "@aws-amplify/ui-react";
 import Chat from "@/components/Chat";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
@@ -7,14 +15,21 @@ export default function Home() {
   const { user, signOut } = useAuthenticator();
 
   return (
-    <div className="app-container">
-      <header className="App-header">
-        <h1>Hello {user?.signInDetails?.loginId}</h1>
-        <Button onClick={signOut}>Sign out</Button>
-      </header>
-      <main>
+    <View className="app-container">
+      <Flex as="header" justifyContent="space-between" alignItems="center" padding="1rem">
+      <Text fontWeight="bold">{user?.signInDetails?.loginId}</Text>
+      <Heading level={3}>Travel Personal Assistant</Heading>
+      <Button
+          onClick={signOut}
+          size="small"
+          variation="link"
+        >
+          Sign out
+        </Button>
+      </Flex>
+      <View as="main">
         <Chat />
-      </main>
-    </div>
+      </View>
+    </View>
   );
 }

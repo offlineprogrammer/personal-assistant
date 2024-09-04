8da462e99896e3d82209420af513e57e27b7ad33
offlineprogrammer <mmalaka@gmail.com>
2024-09-04 12:38:45 -0700

    Format

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
-
 
 export const backend = defineBackend({
   auth,

787b568a1f91363a14547769b091688efe57b4b9
offlineprogrammer <mmalaka@gmail.com>
2024-09-04 11:40:17 -0700

    AI

--- src/components/AuthWrapper.tsx
+++ src/components/AuthWrapper.tsx
@@ -1,13 +1,8 @@
 "use client";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { ReactNode } from "react";
-import { Amplify } from "aws-amplify";
+
 import "@aws-amplify/ui-react/styles.css";
-import outputs from "../../amplify_outputs.json";
-
-Amplify.configure(outputs);
-
-
 
 interface AuthWrapperProps {
   children: ReactNode;
@@ -15,12 +10,6 @@ interface AuthWrapperProps {
 
 export function AuthWrapper({ children }: AuthWrapperProps) {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <>
-          {children}
-        </>
-      )}
-    </Authenticator>
+    <Authenticator>{({ signOut, user }) => <>{children}</>}</Authenticator>
   );
 }
